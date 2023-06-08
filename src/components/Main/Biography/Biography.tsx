e90a5bc03e94eddfd2650e5f7b8e6ff3e37dac02
adnane <adnanbenzo194@gmail.com>
2023-06-08 21:37:05 +0100

    Added bio from backend api

--- src/components/Main/Biography/Biography.tsx
+++ src/components/Main/Biography/Biography.tsx
@@ -1,13 +1,26 @@
-import './styles.css';
+import { useEffect, useState } from "react";
+import "./styles.css";
 const Biography = () => {
+  let [biography, setBiography] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch("https://portfolio.abenazzou.com/api/biography/sorted")
+      .then((response) => response.json())
+      .then((data) => setBiography(data));
+  }, []);
+
   return (
     <div className="biographyContainer">
       <h1 className="title">Biography</h1>
       <div className="biography">
-        Bacalaureat till lear road Lorem Ipsum is simply dummy text of the
-        printing and typesetting industry.Lorem Ipsum is simply dummy text of
-        the printing and typesetting industry.Lorem Ipsum is simply dummy text
-        of the printing and typesetting industry.
+        {biography.map((bio) => {
+          return (
+            <div key={bio.id}>
+              {bio.start_year ? bio.start_year + " - " : ""}
+              {bio.end_year ? bio.end_year : "Now"} : {bio.occupation}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
