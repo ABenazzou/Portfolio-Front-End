9dd53fd90ebd992577ab8972b440787c7a81feb3
adnane <adnanbenzo194@gmail.com>
2023-06-08 13:07:16 +0100

    Added api fetching for description (Main & Secondary)

--- src/components/Main/Description/Description.tsx
+++ src/components/Main/Description/Description.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Button from "../../shared/Button";
 import Avatar from "./Avatar";
 import "./styles.css";
@@ -6,19 +7,27 @@ const forwardPortfolio = () => {
   console.log("Go to portfolio page");
 };
 const Description = () => {
+  let [mainDescription, setMainDescription] = useState(null);
+  let [secondaryDescription, setSecondaryDescription] = useState(null);
+
+  useEffect(() => {
+    fetch("https://portfolio.abenazzou.com/api/section?title=Main Description")
+      .then((response) => response.json())
+      .then((data) => setMainDescription(data.content));
+
+    fetch(
+      "https://portfolio.abenazzou.com/api/section?title=Secondary Description"
+    )
+      .then((response) => response.json())
+      .then((data) => setSecondaryDescription(data.content));
+  }, []);
+
   return (
     <div className="Container">
       <Avatar />
       <div className="subContainer">
-        <div className="descriptionContent">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry.
-        </div>
-        <div className="descriptionContent">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
-        </div>
+        <div className="descriptionContent">{mainDescription}</div>
+        <div className="descriptionContent">{secondaryDescription}</div>
         <div className="portfolioButton">
           <Button buttonText="My Portfolio" handleClick={forwardPortfolio} />
         </div>
