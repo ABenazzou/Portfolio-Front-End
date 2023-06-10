85615c13eb7ee3ea80e454336c5d035718c49fd9
adnane <adnanbenzo194@gmail.com>
2023-06-10 23:32:42 +0100

    Changed projects to use api

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -3,8 +3,21 @@ import Placement from "../shared/Enums";
 import webDevLogo from "../../assets/basketball.png";
 import projectThumbnail from "../../assets/basketball.png";
 import "./styles.css";
+import { useEffect, useState } from "react";
 
 const Projects = () => {
+  let [webDevProjects, setWebDevProjects] = useState<any[]>([]);
+  let [dataScienceProjects, setDataScienceProejcts] = useState<any[]>([]);
+  let [dataEngineeringProejcts, setDataEngineeringProejcts] = useState<any[]>(
+    []
+  );
+
+  useEffect(() => {
+    fetch("https://portfolio.abenazzou.com/api/domain?name=web deva")
+      .then((response) => response.json())
+      .then((data) => setWebDevProjects(data.projects));
+  }, []);
+
   return (
     <div className="Projects">
       <div className="projects-sidebar">
@@ -43,112 +56,30 @@ const Projects = () => {
         <div className="section-container">
           <h1 className="title">Web Development</h1>
           <div className="projectsSubContainer">
-            <Card
-              title="Project 1"
-              thumbnail={projectThumbnail}
-              radius={25}
-              titlePlacement={Placement.Above}
-              height={200}
-              width={300}
-            />
-            <Card
-              title="Project 2"
-              thumbnail={projectThumbnail}
-              radius={25}
-              titlePlacement={Placement.Above}
-              height={200}
-              width={300}
-            />
-            <Card
-              title="Project 3"
-              thumbnail={projectThumbnail}
-              radius={25}
-              height={200}
-              titlePlacement={Placement.Above}
-              width={300}
-            />
-            <Card
-              title="Project 4"
-              thumbnail={projectThumbnail}
-              height={200}
-              radius={25}
-              titlePlacement={Placement.Above}
-              width={300}
-            />
+            {webDevProjects.map((project) => {
+              return (
+                <Card
+                  title={project.name}
+                  thumbnail={projectThumbnail}
+                  radius={25}
+                  titlePlacement={Placement.Above}
+                  height={200}
+                  width={300}
+                />
+              );
+            })}
           </div>
         </div>
         <div className="section-container">
           <h1 className="title">Data Science</h1>
           <div className="projectsSubContainer">
-            <Card
-              title="Project 1"
-              thumbnail={projectThumbnail}
-              radius={25}
-              titlePlacement={Placement.Above}
-              height={200}
-              width={300}
-            />
-            <Card
-              title="Project 2"
-              thumbnail={projectThumbnail}
-              radius={25}
-              titlePlacement={Placement.Above}
-              height={200}
-              width={300}
-            />
-            <Card
-              title="Project 3"
-              thumbnail={projectThumbnail}
-              radius={25}
-              height={200}
-              titlePlacement={Placement.Above}
-              width={300}
-            />
-            <Card
-              title="Project 4"
-              thumbnail={projectThumbnail}
-              height={200}
-              radius={25}
-              titlePlacement={Placement.Above}
-              width={300}
-            />
+           
           </div>
         </div>
         <div className="section-container">
           <h1 className="title">Data Engineering</h1>
           <div className="projectsSubContainer">
-            <Card
-              title="Project 1"
-              thumbnail={projectThumbnail}
-              radius={25}
-              titlePlacement={Placement.Above}
-              height={200}
-              width={300}
-            />
-            <Card
-              title="Project 2"
-              thumbnail={projectThumbnail}
-              radius={25}
-              titlePlacement={Placement.Above}
-              height={200}
-              width={300}
-            />
-            <Card
-              title="Project 3"
-              thumbnail={projectThumbnail}
-              radius={25}
-              height={200}
-              titlePlacement={Placement.Above}
-              width={300}
-            />
-            <Card
-              title="Project 4"
-              thumbnail={projectThumbnail}
-              height={200}
-              radius={25}
-              titlePlacement={Placement.Above}
-              width={300}
-            />
+           
           </div>
         </div>
       </div>
