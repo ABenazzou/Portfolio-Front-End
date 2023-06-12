85d2d5e1bc3d493b2f01a37cde1f15d14f1b3309
adnane <adnanbenzo194@gmail.com>
2023-06-12 13:51:06 +0100

    Add api to social media

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom";
 import "./Footer.css";
+import { useEffect, useState } from "react";
 const Footer = () => {
+  let [socialMedias, setSocialMedias] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch("https://portfolio.abenazzou.com/api/section/type/social_media_link")
+      .then((response) => response.json())
+      .then((data) => setSocialMedias(data));
+  }, []);
+
   return (
     <div className="footer">
       <ul className="social-media-list">
-        <li className="social-media-item">
-          <Link to="/">Social Media 1 Placeholder</Link>
-        </li>
-        <li className="social-media-item">
-          <Link to="/">Social Media 2 Placeholder</Link>
-        </li>
-        <li className="social-media-item">
-          <Link to="/">Social Media 3 Placeholder</Link>
-        </li>
+        {socialMedias.map((socialMedia) => {
+          return (
+            <li key={socialMedia.id} className="social-media-item">
+              <Link to="/">{socialMedia.title}</Link>
+            </li>
+          );
+        })}
       </ul>
       <span className="copyright">Copyright Lorem Ipsum</span>
     </div>
