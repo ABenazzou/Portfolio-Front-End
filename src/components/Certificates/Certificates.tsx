4dad794ec00f33a36867c5b5f0aaec125d2e7588
adnane <adnanbenzo194@gmail.com>
2023-07-06 23:33:28 +0100

    Added certificates onClick event

--- src/components/Certificates/Certificates.tsx
+++ src/components/Certificates/Certificates.tsx
@@ -6,6 +6,11 @@ import { useEffect, useState } from "react";
 
 const Certificates = () => {
   let [certificates, setCertificates] = useState<any[]>([]);
+  const handleCertificateClick = (certificateLink: string) => {
+    console.log(certificateLink);
+    window.open(certificateLink, '_blank', 'noopener,noreferrer');
+  }
+
 
   useEffect(() => {
     fetch("api/certificate/latest")
@@ -19,15 +24,17 @@ const Certificates = () => {
       <div className="certificateSubContainer">
         {certificates.map((certificate) => {
           return (
-            <Card
-              key={certificate.id}
-              title={certificate.name}
-              thumbnail={certificateLogo}
-              radius={50}
-              titlePlacement={Placement.Undefined}
-              height={300}
-              width={500}
-            />
+            <div key={certificate.id} className="certificateCard" onClick={() => handleCertificateClick(certificate.validity_link)}>
+              <Card
+                key={certificate.id}
+                title={certificate.name}
+                thumbnail={certificateLogo}
+                radius={50}
+                titlePlacement={Placement.Undefined}
+                height={300}
+                width={500}
+              />
+            </div>
           );
         })}
       </div>
--- src/components/Main/Certificates/Certificates.tsx
+++ src/components/Main/Certificates/Certificates.tsx
@@ -5,6 +5,10 @@ import "./styles.css";
 import { useEffect, useState } from "react";
 const Certificates = () => {
   let [certificates, setCertificates] = useState<any[]>([]);
+  const handleCertificateClick = (certificateLink: string) => {
+    console.log(certificateLink);
+    window.open(certificateLink, '_blank', 'noopener,noreferrer');
+  }
 
   useEffect(() => {
     fetch("api/certificate/latest")
@@ -18,6 +22,7 @@ const Certificates = () => {
       <div className="certificatesSubContainer">
         {certificates.map((certificate) => {
           return (
+            <div key={certificate.id} className="certificateCard" onClick={() => handleCertificateClick(certificate.validity_link)}>
             <Card
               key={certificate.id}
               title={certificate.name}
@@ -27,6 +32,7 @@ const Certificates = () => {
               height={500}
               width={500}
             />
+            </div>
           );
         })}
       </div>
