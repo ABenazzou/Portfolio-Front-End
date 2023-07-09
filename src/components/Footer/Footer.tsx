74d0d6d36ada6a9920103fc77799efa23f3cc524
adnane <adnanbenzo194@gmail.com>
2023-07-09 20:47:31 +0100

    Rewrite header and footer as react-bootstrap and bootstrap material

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
 import "./Footer.css";
 import { useEffect, useState } from "react";
+import { MDBBtn, MDBContainer, MDBFooter, MDBIcon } from "mdb-react-ui-kit";
+import { Col, Row } from "react-bootstrap";
 const Footer = () => {
   let [socialMedias, setSocialMedias] = useState<any[]>([]);
   let [copyright, setCopyright] = useState(null);
@@ -20,18 +21,43 @@ const Footer = () => {
   }, []);
 
   return (
-    <div className="footer">
-      <ul className="social-media-list">
-        {socialMedias.map((socialMedia) => {
-          return (
-            <li key={socialMedia.id} className="social-media-item" onClick={() => handleClick(socialMedia.content)}>
-              <Link to="/">{socialMedia.title}</Link>
-            </li>
-          );
-        })}
-      </ul>
-      <span className="copyright">{copyright}</span>
-    </div>
+    <MDBFooter className="bg-dark text-white fixed-bottom">
+      <MDBContainer className='p-0 m-1' fluid>
+
+        <Row>
+          <Col md={4}>
+            {socialMedias.map((socialMedia) => {
+              return (
+                <MDBBtn key={socialMedia.id} outline color="light" floating className='m-1 fixedButton' onClickCapture={() => handleClick(socialMedia.content)}>
+                  <MDBIcon fab icon={socialMedia.title} />
+                </MDBBtn>
+              );
+            })}
+          </Col>
+          <Col md={{ span: 4, offset: 4 }} className="d-flex justify-content-center align-items-center">
+            {copyright}
+          </Col>
+
+
+
+        </Row>
+
+      </MDBContainer>
+
+
+    </MDBFooter>
+    // <div className="footer">
+    //   <ul className="social-media-list">
+    //     {socialMedias.map((socialMedia) => {
+    //       return (
+    //         <li key={socialMedia.id} className="social-media-item" onClick={() => handleClick(socialMedia.content)}>
+    //           <Link to="/">{socialMedia.title}</Link>
+    //         </li>
+    //       );
+    //     })}
+    //   </ul>
+    //   <span className="copyright">{copyright}</span>
+    // </div>
   );
 };
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,74 @@
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  let [isDarkMode, setIsDarkMode] = useState(true);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className="header">
-      <ul className="navbar-list">
-        <li className="navbar-item logo">
-          <Link className="link-padding" to="/">Logo Placeholder</Link>
-        </li>
-        <li className="navbar-item">
-          <Link className="link-padding" to="/">Benazzou Adnane</Link>
-        </li>
-        <li className="navbar-item">
-          <Link className="link-padding" to="/projects">Projects</Link>
-        </li>
-        <li className="navbar-item">
-          <Link className="link-padding" to="/certificates">Certificates</Link>
-        </li>
-        <li className="theme-toggle">Theme Placeholder</li>
-        <li className="navbar-item">
-          <Link className="link-padding" to="/resume">Resume</Link>
-        </li>
-        <li className="navbar-item">
-          <Link className="link-padding" to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </div>
+    <Navbar expand="lg" bg="dark" variant="dark" className="fixed-top">
+      <Container >
+        <Navbar.Brand as={Link} to="/">Logo Placeholder</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="w-100 justify-content-between ms-3">
+            <Nav.Item className="nav-hover">
+              <Nav.Link as={Link} to="/">Benazzou Adnane</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="nav-hover">
+              <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="nav-hover">
+              <Nav.Link as={Link} to="/certificates">Certificates</Nav.Link>
+            </Nav.Item>
+
+            <div className="d-flex align-items-center">
+              <Button variant={isDarkMode ? 'light' : 'dark'} onClick={toggleTheme}>
+                Theme Placeholder
+              </Button>
+            </div>
+
+            <Nav.Item className="nav-hover">
+            <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="nav-hover">
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+
+
+      </Container >
+
+    </Navbar >
+    // <div className="header">
+    //   <ul className="navbar-list">
+    //     <li className="navbar-item logo">
+    //       <Link className="link-padding" to="/">Logo Placeholder</Link>
+    //     </li>
+    //     <li className="navbar-item">
+    //       <Link className="link-padding" to="/">Benazzou Adnane</Link>
+    //     </li>
+    //     <li className="navbar-item">
+    //       <Link className="link-padding" to="/projects">Projects</Link>
+    //     </li>
+    //     <li className="navbar-item">
+    //       <Link className="link-padding" to="/certificates">Certificates</Link>
+    //     </li>
+    //     <li className="theme-toggle">Theme Placeholder</li>
+    //     <li className="navbar-item">
+    //       <Link className="link-padding" to="/resume">Resume</Link>
+    //     </li>
+    //     <li className="navbar-item">
+    //       <Link className="link-padding" to="/contact">Contact</Link>
+    //     </li>
+    //   </ul>
+    // </div>
   );
 };
 
