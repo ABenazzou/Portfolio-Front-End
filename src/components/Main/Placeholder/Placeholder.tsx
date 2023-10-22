5973d0f293d2817a8bf169fd221c4cdd50bf2306
abenazzou <adnanbenzo194@gmail.com>
2023-10-22 23:13:04 +0100

    Add login route to placeholder

--- src/components/Main/Placeholder/Placeholder.tsx
+++ src/components/Main/Placeholder/Placeholder.tsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import "./Placeholder.css";
 
 const Placeholder = () => {
+  const navigate = useNavigate();
+
+  const login = () => {
+    navigate("/login");
+  };
+
   return (
     <div className="main">
       <h1>Website under Maintenance!</h1>
+      Admin? <a onClick={login} style={{color: "blue", textDecoration: "underline", cursor: "pointer"}}>Login</a>
     </div>
   );
 };
